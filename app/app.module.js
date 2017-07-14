ede0ba1d88dadf634be300b2b2864559ee3537ec
Alex <alexjeffcott@gmail.com>
2017-07-14 17:49:12 +0200

    Alexs changes 2
    more changes

--- app/app.module.js
+++ app/app.module.js
@@ -1,50 +1,62 @@
 var app = angular.module('my_legal_helper', ['ngRoute'])
 
 .controller('mainController', function($scope, $route, $routeParams, $location) {
-  $scope.appName = "My Legal Helper";
-  $scope.$route = $route;
-  $scope.$location = $location;
-  $scope.$routeParams = $routeParams;
+	$scope.appName = "My Legal Helper";
+	$scope.$route = $route;
+	$scope.$location = $location;
+	$scope.$routeParams = $routeParams;
 })
+
 .controller('landingController', function($scope) {
-	$scope.hideLandingElementBool = true;
-	$scope.hideLandingElement = function(){
-		$scope.hideLandingElementBool = false;
-	}
+	$scope.hideLBool = true;
 })
-.controller('questionnaireController', function($scope) {
-  	$scope.hideElementBool = true;
-	$scope.hideElement = function(){
-		$scope.hideElementBool = false;
+
+.controller('questionnaireController', function($scope, $http) {
+  	$scope.hideQBool = true;
+	//the ansCase object should have the answers from the qsCase object array pushed to it
+	$scope.ansCase = {
+	firstName: "",
+	lastName: "",
+	dateOfBirth: ""
 	}
-});
+	$scope.qsCase = [
+	{qId: "0", }
+	]
+	console.log(angular.toJson($scope.qsCase));
+/*		$http.get("data/qsCase.json").success(function(data){
+		$scope.qsCase = data;
+	});
+	$http.get("data/qsLetter.json").success(function(data){
+		$scope.qsLetter = data;
+	});*/
+})
+
 app.config(function($routeProvider, $locationProvider) {
 	var baseUrl = 'app/components/';
-  $routeProvider
+	$routeProvider
    .when('/questionnaire', {
-    templateUrl: baseUrl+"questionnaire/questionnaire_view.html",
-    controller: "questionnaireController",
-	resolve: {
-      // I will cause a 1 second delay
-      delay: function($q, $timeout) {
-        var delay = $q.defer();
-        $timeout(delay.resolve, 1000);
-        return delay.promise;
-      }
-    }
-  })
-  .when("/home",
-    {
+		templateUrl: baseUrl+"questionnaire/questionnaire_view.html",
+		controller: "questionnaireController",
+		resolve: {
+			// I will cause a 1 second delay
+			delay: function($q, $timeout) {
+				var delay = $q.defer();
+				$timeout(delay.resolve, 1000);
+				return delay.promise;
+			}
+		}
+	})
+	.when("/home",{
         templateUrl: baseUrl+"landing/landing_view.html",
         controller: "landingController",
 		resolve: {
-      // I will cause a 1 second delay
-      delay: function($q, $timeout) {
-        var delay = $q.defer();
-        $timeout(delay.resolve, 1000);
-        return delay.promise;
-      }
-    }
+			// I will cause a 1 second delay
+			delay: function($q, $timeout) {
+				var delay = $q.defer();
+				$timeout(delay.resolve, 1000);
+				return delay.promise;
+			}
+		}
     }).otherwise({ redirectTo: '/home' });
-	 $locationProvider.html5Mode(true);
+	$locationProvider.html5Mode(true);
 });
